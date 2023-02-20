e4d9abec629b7ec14280fb81a2b1ecaad5aa4de0
tsungilin <mt5718214@gmail.com>
2023-02-20 19:41:31 +0800

    fix typo

--- sliding_window/#3_longest_substring_without_repeating_characters/lengthOfLongestSubstring.go
+++ sliding_window/#3_longest_substring_without_repeating_characters/lengthOfLongestSubstring.go
@@ -19,7 +19,7 @@ func lengthOfLongestSubstring(s string) int {
 		if _, ok := hash[ch]; ok && hash[ch] > start {
 			/**
 			* hash[ch] > start 用於判斷目前 sliding window的start位置是否超過hash[ch]的index
-			* 以 "abba" 當遇到重複元素b時, start將會移動到 index 0, 遇到重複a時需檢查hash['a']的index有沒有大於start, 否則start會往回走
+			* 以 "abba" 當遇到重複元素b時, start將會移動到 index 1, 遇到重複a時需檢查hash['a']的index有沒有大於start, 否則start會往回走
 			 */
 			start = hash[ch]
 			fmt.Println("ok", start)
