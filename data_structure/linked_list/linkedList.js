9ea3b9a5c90cdbcc838fcff3166597afd299b7f3
tsungilin <mt5718214@gmail.com>
2023-02-21 22:08:47 +0800

    linked list push

---
+++ data_structure/linked_list/linkedList.js
@@ -0,0 +1,47 @@
+class Node {
+  constructor(value) {
+    this.value = value
+    this.next = null
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null
+    this.length = 0
+  }
+
+  push(value) {
+    let newNode = new Node(value)
+    if (this.head === null) {
+      this.head = newNode
+    } else {
+      let currentNode = this.head
+      while (currentNode.next !== null) {
+        currentNode = currentNode.next
+      }
+      currentNode.next = newNode
+    }
+    this.length++
+  }
+
+  printAll() {
+    if(this.length === 0) {
+      console.log('Nothing in the linked list.');
+      return
+    }
+
+    let currentNode = this.head
+    while(currentNode !== null) {
+      console.log(currentNode.value);
+      currentNode = currentNode.next
+    }
+  }
+}
+
+const myLinkedList = new LinkedList()
+myLinkedList.push("Mike")
+myLinkedList.push("Harry")
+myLinkedList.push("James")
+myLinkedList.push("Kemp")
+myLinkedList.printAll()
