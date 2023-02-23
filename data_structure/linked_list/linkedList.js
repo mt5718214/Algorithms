cb276f1569e89a302d3ab4a923a2b7817a5a903f
tsungilin <mt5718214@gmail.com>
2023-02-23 19:49:32 +0800

    linked list pop

--- data_structure/linked_list/linkedList.js
+++ data_structure/linked_list/linkedList.js
@@ -1,47 +1,69 @@
 class Node {
   constructor(value) {
-    this.value = value
-    this.next = null
+    this.value = value;
+    this.next = null;
   }
 }
 
 class LinkedList {
   constructor() {
-    this.head = null
-    this.length = 0
+    this.head = null;
+    this.length = 0;
   }
 
   push(value) {
-    let newNode = new Node(value)
+    let newNode = new Node(value);
     if (this.head === null) {
-      this.head = newNode
+      this.head = newNode;
     } else {
-      let currentNode = this.head
+      let currentNode = this.head;
       while (currentNode.next !== null) {
-        currentNode = currentNode.next
+        currentNode = currentNode.next;
       }
-      currentNode.next = newNode
+      currentNode.next = newNode;
     }
-    this.length++
+    this.length++;
+  }
+
+  pop() {
+    if (!this.head) {
+      return null;
+    } else if (this.length === 1) {
+      let lastNode = this.head;
+      this.head = null;
+      this.length--;
+      return lastNode;
+    } else {
+      let currentNode = this.head;
+      for (let i = 1; i <= this.length - 2; i++) {
+        currentNode = currentNode.next;
+      }
+      let lastNode = currentNode.next;
+      currentNode.next = null;
+      this.length--;
+      return lastNode;
+    }
   }
 
   printAll() {
-    if(this.length === 0) {
-      console.log('Nothing in the linked list.');
-      return
+    if (this.length === 0) {
+      console.log("Nothing in the linked list.");
+      return;
     }
 
-    let currentNode = this.head
-    while(currentNode !== null) {
+    let currentNode = this.head;
+    while (currentNode !== null) {
       console.log(currentNode.value);
-      currentNode = currentNode.next
+      currentNode = currentNode.next;
     }
   }
 }
 
-const myLinkedList = new LinkedList()
-myLinkedList.push("Mike")
-myLinkedList.push("Harry")
-myLinkedList.push("James")
-myLinkedList.push("Kemp")
-myLinkedList.printAll()
+const myLinkedList = new LinkedList();
+myLinkedList.push("Mike");
+myLinkedList.push("Harry");
+myLinkedList.push("James");
+myLinkedList.push("Kemp");
+myLinkedList.printAll();
+
+console.log(myLinkedList.pop());
