ea36dd2cfe8d8d457a4f8af978de5a5b8e91517d
tsungilin <mt5718214@gmail.com>
2023-07-24 09:48:48 +0800

    update maxSubArray

--- sliding_window/#53_maximum_subarray/maxSubArray.js
+++ sliding_window/#53_maximum_subarray/maxSubArray.js
@@ -24,3 +24,28 @@ var maxSubArray = function (nums) {
 maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4])  // 6
 maxSubArray([1]) // 1
 maxSubArray([5, 4, -1, 7, 8]) // 23
+
+var maxSubArray1 = function (nums) { // O(n^2)
+  let max = -Infinity
+  
+  for(let i = 0; i < nums.length; i++) {
+    let currsum = 0
+    currsum += nums[i]
+    if (currsum > max) {
+      max = currsum
+    }
+    for(let j = i+1; j < nums.length; j++) {
+      currsum += nums[j]
+      
+      if(currsum > max) {
+        max = currsum
+      }
+    }
+  }
+  console.log(max);
+  return max
+};
+
+maxSubArray1([-2, 1, -3, 4, -1, 2, 1, -5, 4])  // 6
+maxSubArray1([1]) // 1
+maxSubArray1([5, 4, -1, 7, 8]) // 23
