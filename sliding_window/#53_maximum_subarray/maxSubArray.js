c8ea63049b01e0341fdba513059349a190588fd5
tsungilin <mt5718214@gmail.com>
2023-07-24 17:27:18 +0800

    update maxSubArray

--- sliding_window/#53_maximum_subarray/maxSubArray.js
+++ sliding_window/#53_maximum_subarray/maxSubArray.js
@@ -8,7 +8,7 @@ var maxSubArray = function (nums) {
   for(let num of nums) {
     currsum += num
     if (currsum < num) {
-      // 檢查目前的累積值是否比當前數字小, 如果是那目前的最大值就會是當前數字
+      // 檢查目前的累積值是否比當前數字小(加完第n個數後,還比較小就表示前n-1個總和是負的), 如果是那目前的最大值就會是當前數字
       currsum = num
     }
 
@@ -49,3 +49,24 @@ var maxSubArray1 = function (nums) { // O(n^2)
 maxSubArray1([-2, 1, -3, 4, -1, 2, 1, -5, 4])  // 6
 maxSubArray1([1]) // 1
 maxSubArray1([5, 4, -1, 7, 8]) // 23
+
+var maxSubArray2 = function (nums) { // O(n)
+  let max = nums[0]
+  let currsum = nums[0]
+
+  for (let i = 1; i < nums.length; i++) {
+    if(currsum < 0) {
+      currsum = nums[i]
+    } else {
+      currsum += nums[i]
+    }
+    max = Math.max(currsum, max)
+  }
+
+  console.log(max);
+  return max
+};
+
+maxSubArray2([-2, 1, -3, 4, -1, 2, 1, -5, 4])  // 6
+maxSubArray2([1]) // 1
+maxSubArray2([5, 4, -1, 7, 8]) // 23
